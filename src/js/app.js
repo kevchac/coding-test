56acb9423f2f48f6c64952c0aaae4128da103ea0
Kevin Chamorro Cruz <kevin.chamorro.cruz@accenture.com>
2022-02-25 20:41:27 -0600

    Last changes about all the files, added some changes

--- src/js/app.js
+++ src/js/app.js
@@ -1,19 +1,20 @@
+//Main variables
+const tabHeader = document.querySelectorAll('.list__link');
+const listIndicator = document.querySelectorAll('.list__bar');
+const sectionContainer = document.querySelectorAll('.container');
 
-let tabHeader = document.querySelectorAll('.list__link');
-let listIndicator = document.querySelectorAll('.list__bar');
 
-let sectionContainer = document.querySelectorAll('.container');
-
-
+//Iterate the different arrays of tab elements
 for (let index = 0; index < tabHeader.length; index++) {
     tabHeader[index].addEventListener('click', function () {
+        //Get active elements and delete active class
         document.querySelector('.list__link--active').classList.remove('list__link--active')
         document.querySelector('.list__bar--active').classList.remove('list__bar--active');
+        
+        //Add new active elements
         tabHeader[index].classList.add('list__link--active');
         listIndicator[index].classList.add('list__bar--active');
-
         document.querySelector('.container--active').classList.remove('container--active');
         sectionContainer[index].classList.add('container--active');
     })
-    
 }
